913a037849303cf1337c96b5e58dec20fa58e182
Vidotto <vitotbrbr@gmail.com>
2024-10-17 21:48:13 -0300

    hotfix any types

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
-const NavLink = ({ href, title }) => {
+type NavLinkProps = {
+  href: string;
+  title: string;
+};
+
+const NavLink: React.FC<NavLinkProps> = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-[#ffffff] sm:text-xl rounded md:p-0 hover:text-[#]"
+      className="block py-2 pl-3 pr-4 text-[#ffffff] sm:text-xl rounded md:p-0 hover:text-[#ffcc00]"
     >
       {title}
     </Link>

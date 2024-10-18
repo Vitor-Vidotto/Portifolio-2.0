9b0d4af79996be69e735292b30f418f8a4a86eb4
Vidotto <vitotbrbr@gmail.com>
2024-10-17 22:01:51 -0300

    teste 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Se você deseja exportar como um site estático
+    experimental: {
+      appDir: true, // Habilitar App Directory se você estiver usando Next.js 13+
+    },
+    telemetry: false, // Desativar a coleta de dados anônimos
+  };
+  
+  export default nextConfig;
+  

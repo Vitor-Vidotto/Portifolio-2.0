a6ec14c51760e3a38e01acaa18b83bb944a44198
Vidotto <vitotbrbr@gmail.com>
2024-10-18 13:55:41 -0300

    test

--- app/quem-sou/page.tsx
+++ app/quem-sou/page.tsx
@@ -1,18 +1,14 @@
 import Navbar from "../components/NavBars";
-import Timeline from "../components/TimeLine"; // Importando o componente Timeline
-import AboutSection from "../components/AboutSection";
-import ProjectsSectionPortifolio from "../components/ProjectsPortifolio";
 import ThreeDScene from "../components/ThreeDScene";
+import ContatoSection from "../components/ContatoSection";
+
 
 export default function QuemSou() {
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        <AboutSection />
-        <Timeline />
-        <ProjectsSectionPortifolio />
-
+        <ContatoSection />
       </div>
       <a
   href="https://www.linkedin.com/in/vitor-vidotto"

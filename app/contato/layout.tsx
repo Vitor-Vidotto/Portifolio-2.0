68892358ad4c7ec0fd1f5e32a24272d17ea8ff24
Vidotto <vitotbrbr@gmail.com>
2024-10-17 22:30:56 -0300

    hotfix

--- app/contato/layout.tsx
+++ app/contato/layout.tsx
@@ -1,15 +1,22 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react'; // Importando ReactNode
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "Vitor Vidotto",
   description: "Portifólio Vitor Vidotto",
 };
-export default function RootLayout({ children }) {
+
+// Definindo a interface para as props do componente
+interface RootLayoutProps {
+  children: ReactNode; // Tipando children como ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

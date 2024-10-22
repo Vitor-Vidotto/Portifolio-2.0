a27a7ae1b17f317252ef2c605046a9e22bf88870
Vidotto <vitotbrbr@gmail.com>
2024-10-21 21:35:45 -0300

    undo tests

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          src="/public/Vitor Vidotto.png"
+          src="/Vitor Vidotto.png"
           alt="Vitor Vidotto Logo"
           width={320}
           height={48}

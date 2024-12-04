4d0eb391ebd02306ebbac4fe205ad8b793b92fa6
Vitor-Vidotto <vitotbrbr@gmail.com>
2024-12-04 11:27:19 -0300

    frontpage css update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,18 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' }, // '0' como string
+          '100%': { opacity: '1' }, // '1' como string
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
